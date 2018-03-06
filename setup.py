7995d4cfbfe5f41b9216ff87259d7fed7ccbadea
fatz <fabrice.tz@51stfloor.com>
2018-03-06 11:20:43 +0100

    dist to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='obidrfid',
+    packages=['obidrfid'],
+    version='0.1',
+    description='Python wrapper to communicate with eh OBID RFID IP antennas',
+    author='Fabrice Tereszkiewicz',
+    author_email='fabrice.tz@51stfloor.com',
+    url='https://github.com/fatzh/obidrfid',
+    download_url='https://github.com/fatzh/obidrfid/archive/0.1.tar.gz',
+    keywords=['rfid', 'obid'],
+    classifiers=[],
+)
