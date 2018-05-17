4e32bb29ae3294c5f92f8c0ab8fb66c83f5df848
fatz <fabrice.tz@51stfloor.com>
2018-05-17 12:15:49 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='obidrfid',
     py_modules=['obidrfid'],
     scripts=['obidrfid.py'],
-    version='1.1',
+    version='1.2',
     description='Python wrapper to communicate with eh OBID RFID IP antennas',
     author='Fabrice Tereszkiewicz',
     author_email='fabrice.tz@51stfloor.com',
